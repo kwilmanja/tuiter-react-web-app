559557f1858d54d746fcbf0ea07374cb8507821d
kwilmanja <kwilman.j@northeastern.edu>
2023-03-05 22:23:01 -0500

    a6 React.js sp23

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
         <BrowserRouter>
             <div className="container">
-                <Routes>
+                <Routes> 
                     <Route index
                            element={<Labs/>}/>
                     <Route path="/hello"
